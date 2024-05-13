c49c4ae0e960b16240339d752ab0a1a4b2162a74
rudmarv <rudmarv@gmail.com>
2024-05-14 05:43:54 +0800

    2nd

--- index.php
+++ index.php
@@ -5,26 +5,20 @@ echo "hello world";
 require 'vendor/autoload.php';
 
 use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
 
-// Create an S3 client
 $s3 = new S3Client([
-    'version' => '2006-03-01',
-    'region'  => 'us-east-1', // Change to your desired region
+    'region' => 'us_east_1',
+    'version' => 'latest',
+    'credentials' => [
+        'key'    => 'your_access_key',
+        'secret' => 'your_secret_key',
+    ],
 ]);
 
-try {
-    // List S3 buckets
-    $buckets = $s3->listBuckets();
+$result = $s3->listBuckets();
 
-    // Display bucket names
-    echo "<h2>Amazon S3 Buckets:</h2>";
-    foreach ($buckets['Buckets'] as $bucket) {
-        echo $bucket['Name'] . "<br>";
-    }
-} catch (AwsException $e) {
-    // Display error message if any
-    echo "Error: " . $e->getAwsErrorMessage();
+foreach ($result['Buckets'] as $bucket) {
+    echo $bucket['Name'] . PHP_EOL;
 }
 
 ?>
