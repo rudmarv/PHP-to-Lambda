7c613c3b5c544b2e16092a39bb525e0610804c1a
rudmarv <rudmarv@gmail.com>
2024-05-14 08:27:10 +0800

    clean

--- index.php
+++ index.php
@@ -2,23 +2,4 @@
 
 echo "hello world";
 
-require 'vendor/autoload.php';
-
-use Aws\S3\S3Client;
-
-$s3 = new S3Client([
-    'region' => 'us_east_1',
-    'version' => 'latest',
-    'credentials' => [
-        'key'    => 'your_access_key',
-        'secret' => 'your_secret_key',
-    ],
-]);
-
-$result = $s3->listBuckets();
-
-foreach ($result['Buckets'] as $bucket) {
-    echo $bucket['Name'] . PHP_EOL;
-}
-
 ?>
